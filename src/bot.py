6269fa0cf106c574f61193f6e059cb9726cbe604
Jean Estevez <ctarriba9@gmail.com>
2025-06-26 12:38:33 -0400

    commands, logger

--- src/bot.py
+++ src/bot.py
@@ -4,13 +4,41 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 from googletrans import Translator, LANGUAGES
 
 TOKEN = "TELEGRAM TOKEN, from @BotFather"
+translator = Translator()
 
 # -----------------------------------------------------------------------------
 # Logging settings
 logger = logging.getLogger(__name__)
-logging.basicConfig()
+logging.basicConfig(
+    format ='%(actime)s - %(name)s - %(levelname)s - %(message)s',
+    level = logging.INFO,
+    handlers = [
+        logging.FileHandler("translation_bot_INFO.log"),
+        logging.StreamHandler()
+    ]
+)
 
+# -----------------------------------------------------------------------------
+# Bot Commands
 
 def start() -> None:
     user = Update.effective_user
+    logger.info(f"User {user.id} start conversation")
+
     Update.message.reply_text(f"Hi {user.first_name}! I am LinguaSpark Bot...")
+
+def help_command(Update, context: CallbackContext) -> None:
+    help_text = """
+        XD *Help For LinguaSpark Bot :P 
+    """
+    Update.message.reply_text(help_text, parse_mode="Markdown")
+
+def lang_command() -> None:
+    pass
+
+def lang_callback() -> None:
+    pass
+
+def info_command() -> None:
+    pass
+
